ac9f9695e403decb171ef9c1887be4d76cbc7213
Torch <rustruedetective@gmail.com>
2024-12-27 22:43:12 +0500

    added space

--- queries/views/KPIs/1_TotalStock.sql
+++ queries/views/KPIs/1_TotalStock.sql
@@ -1,7 +1,9 @@
-create view totalstocksum as 
+create view total_stock_sum as 
 select sum(quantity)
 from production.productinventory;
 
-create view totalstock as
+
+
+create view total_stock as
 select productid, quantity
 from production.productinventory;
